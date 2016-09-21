bd5ecd4097d39f14d644f92ea8efb673a2166c80
mateusz <mateusz.jendza@outlook.com>
2016-09-21 17:30:53 +0200

    added JavaScript generation via controller

---
+++ API/Controllers/JavaScriptGeneratorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace API.Controllers
+{
+    public class JavaScriptGeneratorController : ApiController
+    {
+        [EnableTag]
+        [HttpGet]
+        public ResponseMessageResult Get()
+        {
+            var str = "var abc='a'";
+
+            var response = ResponseMessage(new HttpResponseMessage()
+            {
+                Content = new StringContent(str, Encoding.UTF8, "application/javascript"),
+                StatusCode = HttpStatusCode.OK,
+            });
+
+            return response;
+        }
+    }
+}
